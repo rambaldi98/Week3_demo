a85c40a68736eb2d0b0e40816551fda97c2b4d8a
rambaldi <diencong5998@gmail.com>
2021-04-12 15:25:32 +0700

    add file employee.java

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+public class Employee {
+    private String name;
+    private String code;
+    private String sdt;
+    private String email;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
