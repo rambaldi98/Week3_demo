9feb7571c7264018502d0b0b6209c4e1398771d5
rambaldi <diencong5998@gmail.com>
2021-04-12 15:26:45 +0700

    add file partime employee.java

---
+++ src/PartimeEmployee.java
@@ -0,0 +1,11 @@
+public class PartimeEmployee  extends Employee{
+    private int timework;
+
+    public int getTimework() {
+        return timework;
+    }
+
+    public void setTimework(int timework) {
+        this.timework = timework;
+    }
+}
